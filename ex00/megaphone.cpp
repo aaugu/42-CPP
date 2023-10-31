bffc802a005abbe7a058f5b491ad96402f2451e5
Anais Augu <aaugu@c2r9s5.42lausanne.ch>
2023-10-31 13:46:02 +0100

    ex00 ok + rename of files in ex01

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,11 +6,22 @@
 /*   By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/30 18:31:10 by aaugu             #+#    #+#             */
-/*   Updated: 2023/10/30 18:31:25 by aaugu            ###   ########.fr       */
+/*   Updated: 2023/10/31 13:45:14 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	main(void)
+#include <iostream>
+#include <algorithm>
+
+int	main(int ac, char **av)
 {
+	if (ac == 1)
+		return ((std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl), 0);
+	for (int i = 1; i < ac; i++)
+	{
+		for (int j = 0; j < (int)strlen(av[i]); j++)
+			std::cout << (char)toupper(av[i][j]);
+	}
+	std::cout << std::endl;
 	return (0);
 }
