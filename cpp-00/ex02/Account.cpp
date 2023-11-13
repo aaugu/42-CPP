66d2c95f93c071c849fda453f234ad101bb76a5b
Anais Augu <augu.anais@gmail.com>
2023-11-13 17:09:39 +0100

    cpp-00 finished

--- cpp-00/ex02/Account.cpp
+++ cpp-00/ex02/Account.cpp
@@ -6,23 +6,59 @@
 /*   By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 13:58:13 by aaugu             #+#    #+#             */
-/*   Updated: 2023/11/07 14:37:55 by aaugu            ###   ########.fr       */
+/*   Updated: 2023/11/13 17:02:47 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <ctime>
 #include <iostream>
+#include "Account.hpp"
 
 /* ************************************************************************** */
-/*                          CONSTRUCTOR & DESTRUCTOR                          */
+/*                         CONSTRUCTORS & DESTRUCTOR                          */
 /* ************************************************************************** */
+
+// Initialize
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+Account::Account(void)
+{
+	Account::_displayTimestamp();
+	this->_accountIndex = _nbAccounts;
+	this->_amount = 0;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	Account::_nbAccounts++;
+	std::cout	<< "index:" << this->_accountIndex << ";"
+				<< "amount:" << this->_amount << ";"
+				<< "created" << std::endl;
+}
 
 Account::Account(int initial_deposit)
 {
+	Account::_displayTimestamp();
+	this->_accountIndex = _nbAccounts;
+	this->_amount = initial_deposit;
+	this->_nbDeposits = 0;
+	this->_nbWithdrawals = 0;
+	Account::_nbAccounts++;
+	Account::_totalAmount += initial_deposit;
+	std::cout	<< "index:" << this->_accountIndex << ";"
+				<< "amount:" << this->_amount << ";"
+				<< "created" << std::endl;
 }
 
 Account::~Account(void)
 {
+	Account::_displayTimestamp();
+	Account::_nbAccounts--;
+	Account::_totalAmount -= this->_amount;
+	std::cout	<< "index:" << this->_accountIndex << ";"
+				<< "amount:" << this->_amount << ";"
+				<< "closed" << std::endl;
 }
 
 /* ************************************************************************** */
@@ -31,75 +67,120 @@ Account::~Account(void)
 
 void	Account::makeDeposit(int deposit)
 {
+	if (deposit < 0)
+		return ;
+	_displayTimestamp();
+	std::cout	<< "index:" << this->_accountIndex << ";"
+				<< "p_amount:" << this->_amount << ";"
+				<< "deposit:" << deposit << ";";
+	this->_amount += deposit;
+	Account::_totalAmount += deposit;
+	this->_nbDeposits++;
+	Account::_totalNbDeposits++;
+	std::cout	<< "amount:" << this->_amount << ";"
+				<< "nb_deposit:" << this->_nbDeposits << std::endl;
+
 }
 
 bool	Account::makeWithdrawal(int withdrawal)
 {
+	Account::_displayTimestamp();
+	std::cout	<< "index:" << this->_accountIndex << ";"
+				<< "p_amount:" << this->_amount << ";"
+				<< "withdrawal:";
+
+	if (withdrawal > this->_amount)
+		return (std::cout << "refused" << std::endl, false);
+	else
+	{
+		this->_amount -= withdrawal;
+		Account::_totalAmount -= withdrawal;
+		this->_nbWithdrawals++;
+		Account::_totalNbWithdrawals++;
+		std::cout	<< withdrawal << ";"
+					<< "amount:" << this->_amount << ";"
+					<< "nb_withdrawal:" << this->_nbWithdrawals << std::endl;
+		return (true);
+	}
 }
 
 int		Account::checkAmount(void) const
 {
+	return (this->_amount);
 }
 
 void	Account::displayStatus(void) const
 {
+	Account::_displayTimestamp();
+	std::cout	<< "index:" << this->_accountIndex << ";"
+				<< "amount:" << this->_amount << ";"
+				<< "deposits:" << this->_nbDeposits << ";"
+				<< "withdrawals:" << this->_nbWithdrawals
+				<< std::endl;
 }
 
-static void	Account::displayAccountsInfos( void )
+void	Account::displayAccountsInfos( void )
 {
+	Account::_displayTimestamp();
+	std::cout	<< "accounts:" << Account::_nbAccounts << ";"
+				<< "total:" << Account::_totalAmount << ";"
+				<< "deposits:" << Account::_totalNbDeposits << ";"
+				<< "withdrawals:" << Account::_totalNbWithdrawals
+				<< std::endl;
 }
 
 /* ************************************************************************** */
 /*                                  GETTERS                                   */
 /* ************************************************************************** */
 
-static int	Account::getNbAccounts(void)
+int	Account::getNbAccounts(void)
 {
+	return (_nbAccounts);
 }
 
-static int	Account::getTotalAmount(void)
+int	Account::getTotalAmount(void)
 {
+	return (_totalAmount);
 }
 
-static int	Account::getNbDeposits(void)
+int	Account::getNbDeposits(void)
 {
+	return (_totalNbDeposits);
 }
 
-static int	Account::getNbWithdrawals(void)
+int	Account::getNbWithdrawals(void)
 {
+	return (_totalNbWithdrawals);
 }
 
 /* ************************************************************************** */
 /*                             PRIVATE FUNCTIONS                              */
 /* ************************************************************************** */
 
-static void	_displayTimestamp( void )
+void	Account::_displayTimestamp( void )
 {
-	time_t	now = time(0);
-	tm		*ltm = localtime(&now);
+	time_t	now = std::time(0);
+	tm		*time = std::localtime(&now);
 
-	std::cout << "[" << 1900 + ltm->tm_year;
-	if (ltm->tm_mon + 1 >= 10)
-		std::cout << ltm->tm_mon + 1;
-	else
-		std::cout << "0" << ltm->tm_mon + 1;
-	if (ltm->tm_mday >= 10)
-		std::cout << ltm->tm_mday;
-	else
-		std::cout << "0" << ltm->tm_mday;
-	std::cout << "_";
-	if (ltm->tm_hour >= 10)
-		std::cout << ltm->tm_hour;
-	else
-		std::cout << "0" << ltm->tm_hour;
-	if (ltm->tm_min >= 10)
-		std::cout << ltm->tm_min;
-	else
-		std::cout << "0" << ltm->tm_min;
-	if (ltm->tm_sec >= 10)
-		std::cout << ltm->tm_sec;
-	else
-		std::cout << "0" << ltm->tm_sec;
-	std::cout << "] ";
-	return (0);
+	std::cout << "[" << 1900 + time->tm_year;
+
+	if (time->tm_mon + 1 < 10)
+		std::cout << "0";
+	std::cout << time->tm_mon + 1;
+
+	if (time->tm_mday < 10)
+		std::cout << "0";
+	std::cout << time->tm_mday << "_";
+
+	if (time->tm_hour < 10)
+		std::cout << "0";
+	std::cout << time->tm_hour;
+
+	if (time->tm_min < 10)
+		std::cout << "0";
+	std::cout << time->tm_min;
+
+	if (time->tm_sec < 10)
+		std::cout << "0";
+	std::cout << time->tm_sec << "] ";
 }
