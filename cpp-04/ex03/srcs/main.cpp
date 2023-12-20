59334c529dcfdb1686ebf050b64c9c22b920f5be
Anais Augu <augu.anais@gmail.com>
2023-12-20 17:16:14 +0100

    cpp 04 ex03 initialized with main and Makefile fixed

--- cpp-04/ex03/srcs/main.cpp
+++ cpp-04/ex03/srcs/main.cpp
@@ -6,10 +6,15 @@
 /*   By: aaugu <aaugu@student.42lausanne.ch>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/20 17:05:58 by aaugu             #+#    #+#             */
-/*   Updated: 2023/12/20 17:06:55 by aaugu            ###   ########.fr       */
+/*   Updated: 2023/12/20 17:15:26 by aaugu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/AMateria.hpp"
 #include "../includes/ICharacter.hpp"
 #include "../includes/IMateriaSource.hpp"
+
+int	main(void)
+{
+	return (0);
+}
